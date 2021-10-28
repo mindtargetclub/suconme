9fed6f816fde4ddd3039a63457d9eb9585ec5a17
s9921316 <audiowalk.art@gmail.com>
2021-10-28 16:33:18 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,27 @@
+// const config = {
+//   publicPath: "./",
+//   configureWebpack: { 
+//     output: {
+//       libraryExport: 'default'
+//     }
+//   }
+// }
+
+// if (process.env.NODE_ENV === "production") {
+//   config.configureWebpack.externals = {
+//     sortablejs: {
+//       commonjs: "sortablejs",
+//       commonjs2: "sortablejs",
+//       amd: "sortablejs",
+//       root: "Sortable"
+//     }
+//   };
+// };
+
+// module.exports = config;
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/suconme/'
+    : '/'
+}
