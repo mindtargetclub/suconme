a7fdde7a173bccc1f7d5f6fe2ce3637b322d9832
s9921316 <audiowalk.art@gmail.com>
2021-10-28 16:36:58 +0800

    ok
    Description ..

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
- 
+
 #!/usr/bin/env sh
 # 當發生錯誤時終止腳本運行
 set -e
@@ -6,10 +6,10 @@ set -e
 npm run build
 # 移動至到打包後的dist目錄 
 cd dist
-git init  
+git init 
 git add -A
 git commit -m 'deploy'
 # 部署到 https://github.com/chou0728/eric-project.git 分支為 gh-pages
 git push -f https://github.com/mindtargetclub/suconme.git master:gh-pages
- 
+
 cd -
